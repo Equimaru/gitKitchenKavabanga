42eed6be937ce5b959b38704e36e2e9ec32b1fd2
Equimaru <119777912+Equimaru@users.noreply.github.com>
2023-12-14 02:38:42 +0300

    Bunch of a ReferenceExeptions to solve
    As a wise man would say: "Don't touch if it works, otherwise it won't."

--- KitchenKavabanga/Assets/C# Scripts/KitchenObject.cs
+++ KitchenKavabanga/Assets/C# Scripts/KitchenObject.cs
@@ -6,11 +6,38 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
 
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
     }
 
+    //After triggering function to clear parent for object it check if there is one
+    //public void SetClearCounter(ClearCounter clearCounter) 
+    //{
+        //if (clearCounter != null)
+        //{
+            //If there is one we clear parent
+            //this.clearCounter.ClearKitchenObject();
+        //}
+            //And set new one after
+        //this.clearCounter = clearCounter;
 
+        //if (clearCounter.HasKitchenObject())
+        //{
+            //Debug.LogError("Counter already has KitchenObject!!");
+        //}
+
+        //clearCounter.SetKitchenObject(this);
+
+        //transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        //transform.localPosition = Vector3.zero;
+    //}
+    
+    //public ClearCounter GetClearCounter() 
+    //{ 
+        //return clearCounter; 
+    //}
 }
