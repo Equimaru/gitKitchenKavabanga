cfeb2935fa36129cfbe37fd4a3007b88f348127b
Equimaru <119777912+Equimaru@users.noreply.github.com>
2023-12-13 16:30:01 +0300

    Completed Kitchen Object, Scriptable Objects

--- KitchenKavabanga/Assets/C# Scripts/Player.cs
+++ KitchenKavabanga/Assets/C# Scripts/Player.cs
@@ -95,7 +95,7 @@ public class Player : MonoBehaviour
         {
             SetSelectedCounter(null);
         }
-        Debug.Log(selectedCounter);
+        //Debug.Log(selectedCounter);
     }
     private void HandleMovement()
     {
